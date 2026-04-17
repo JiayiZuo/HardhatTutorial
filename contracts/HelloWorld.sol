5fcf43e646582f28f3027d1902d3318a53bb95ef
ZUO Yi <yizuo@cmi.local>
2026-04-17 09:59:12 +0800

    hardhat tutorial

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import { BaseContract } from "./BaseContract.sol";
+
+contract HelloWorld is BaseContract {
+  string public greet = "Hello World!";
+}
